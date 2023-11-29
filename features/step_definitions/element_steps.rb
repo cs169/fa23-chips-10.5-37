47a1617c20b5c96da86921b6f749b506e4b7bed5
Kendall Schubert <schubertk@berkeley.edu>
2023-11-29 06:01:30 +0000

    modify capybara test structure for lint

--- features/step_definitions/element_steps.rb
+++ features/step_definitions/element_steps.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 # step defs for elements within the pages
 
 require 'uri'
 require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
-
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
 
 Then(/^the table should have (\d+) rows$/) do |expected_rows|
   within('table') do
--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,21 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'spec_helper'
 
 describe Representative do
-  before :each do
-    rep = Representative.create!(name: 'First Last', ocdid: 'ocd-division/country:us', title: 'Judge')
+  it 'creates a valid representative' do
+    rep = described_class.new(name: 'First Last', ocdid: 'ocd-division/country:us', title: 'Judge')
+    expect(rep).to be_valid
   end
-
-  describe 'valid representative with assigned attributes'
-    it 'creates a valid representative' do
-      expect(rep).to be_valid
-    end
-    # it 'does not create duplicate' do
-    #   expect(rep).not_to receive(:create)
-    # end
-  end
-
-  
-    
-
 end
+# it 'does not create duplicate' do
+#   expect(rep).not_to receive(:create!)
+# end
