12deb723fcf2fc7510bf856331c0fc3dc4bc3af5
Naia Johnson <masako_@berkeley.edu>
2023-12-02 01:41:04 +0000

    more tests added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,8 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
   describe 'user init method tests' do
-    let(:user1) { described_class.new(first_name: 'Jim', last_name: 'Carrey', 
-    provider: 'google_oauth2', uid: '889') }
-    let(:user2) { described_class.new(first_name: 'Carrey', last_name: 'Jim', 
-    provider: 'github', uid: '889') }
+    let(:user1) { described_class.create(first_name: 'Jim', last_name: 'Carrey', provider: 'google_oauth2', uid: '420') }
+    let(:user2) { described_class.create(first_name: 'Carrey', last_name: 'Jim', provider: 'github', uid: '069') }
 
     it 'returns the full name' do
       expect(user1.name).to eq('Jim Carrey')
@@ -20,18 +18,33 @@ RSpec.describe User, type: :model do
       expect(user2.auth_provider).to eq('Github')
     end
   end
-end
+
+  describe 'find the user with the uid' do
+    it 'finds github with UID' do
+      git_user = described_class.create(provider: 'github', uid: '123')
+      found = described_class.find_github_user('123')
+      expect(found).to eq(git_user)
+    end
+
+    it 'finds google with UID' do
+      goo_user = described_class.create(provider: 'google_oauth2', uid: '456')
+      found = described_class.find_google_user('456')
+      expect(found).to eq(goo_user)
+    end
+  end
+
 # describe 'find user method tests' do
-    # let(:github_user) { described_class.create(first_name: 'git', last_name: 'hub', provider: 'github', uid: '420') }
-    # let(:google_user) { described_class.create(first_name: 'google', last_name: 'auth', provider: 'google_oauth2', uid: '069') }
+  # let(:github_user) { described_class.create(first_name: 'git', last_name: 'hub', provider: 'github', uid: '420') }
+  # let(:google_user) { described_class.create(first_name: 'google', last_name: 'auth', provider: 'google_oauth2', uid: '069') }
 
-    # it 'finds github with UID' do
-      # found = described_class.find_github_user('420')
-      # expect(found).to eq(github_user)
-    # end
-
-    # it 'finds google with UID' do
-      # found = described_class.find_google_user('069')
-      # expect(found).to eq(google_user)
-    # end
+  # it 'finds github with UID' do
+    # found = described_class.find_github_user('420')
+    # expect(found).to eq(github_user)
   # end
+
+  # it 'finds google with UID' do
+    # found = described_class.find_google_user('069')
+    # expect(found).to eq(google_user)
+  # end
+# end
+end
