dc472a4cb26d4a98f92a66c4f07c04994d8fc2b7
Kendall Schubert <schubertk@berkeley.edu>
2023-12-02 04:33:39 +0000

    pronto bad

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,18 +23,24 @@ RSpec.describe User, type: :model do
     #   expect(user2.auth_provider).to eq('Github')
     # end
   end
+  # rubocop:disable RSpec/ExampleLength
 
   describe 'find the user with the uid' do
-    it 'finds github with UID' do
+    it 'finds github and google from UID' do
       git_user = described_class.create(provider: 'github', uid: '123')
       found = described_class.find_github_user('123')
       expect(found).to eq(git_user)
-    end
 
-    it 'finds google with UID' do
       goo_user = described_class.create(provider: 'google_oauth2', uid: '456')
       found = described_class.find_google_user('456')
       expect(found).to eq(goo_user)
     end
+    # rubocop:enable RSpec/ExampleLength
+
+    # it 'finds google with UID' do
+    #   goo_user = described_class.create(provider: 'google_oauth2', uid: '456')
+    #   found = described_class.find_google_user('456')
+    #   expect(found).to eq(goo_user)
+    # end
   end
 end
