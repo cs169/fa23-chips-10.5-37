5120a77546e52094ccf8c4014ee9dfcf406aa881
Kendall Schubert <schubertk@berkeley.edu>
2023-12-02 04:25:22 +0000

    pronto is forcing me to make bad styling choices

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,15 +10,18 @@ RSpec.describe User, type: :model do
     end
     let(:user2) { described_class.create(first_name: 'Carrey', last_name: 'Jim', provider: 'github', uid: '069') }
 
-    it 'returns the full name' do
+    it 'returns the full name and authority provider' do
       expect(user1.name).to eq('Jim Carrey')
       expect(user2.name).to eq('Carrey Jim')
-    end
 
-    it 'returns the autho provider' do
       expect(user1.auth_provider).to eq('Google')
       expect(user2.auth_provider).to eq('Github')
     end
+
+    # it 'returns the autho provider' do
+    #   expect(user1.auth_provider).to eq('Google')
+    #   expect(user2.auth_provider).to eq('Github')
+    # end
   end
 
   describe 'find the user with the uid' do
