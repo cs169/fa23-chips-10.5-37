5970e4aa2cc02f936c98c19044cac44fcafbd910
Kendall Schubert <schubertk@berkeley.edu>
2023-12-09 07:23:06 +0000

    2.3/4/5 sorta done

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -15,11 +15,25 @@ class MyNewsItemsController < SessionController
   def edit; end
 
   def create
-    # @news_item = NewsItem.new(news_item_params)
-    @representative = Representative.find(params[:representative_id])
-    # @news_item = @representative.news_items.build(news_item_params)
-    @news_item = NewsItem.new
-
+    @news_item = NewsItem.new(news_item_params)
+    # selected_index = params[:selected_article_index]
+    # selected_article = params[:articles][selected_index]
+    # @news_item = NewsItem.new
+    # @news_item = NewsItem.new(
+    #     title: selected_article[:title],
+    #     description: selected_article[:description],
+    #     link: selected_article[:link],
+    #     representative_id: params[:news_item][:representative_id],
+    #     issue: params[:news_item][:issue]
+    # )
+    # @news_item = NewsItem.new
+    # @news_item.representative_id = params[:representative_id]
+    # @news_item.issue = params[:issue]
+    # @news_item.title = params[:title]
+    # @news_item.link = params[:link]
+    # @news_item.description = params[:description]
+    # puts @news_item
+    # @news_item.rating = params[:rating]
     if @news_item.save
       redirect_to representative_news_item_path(@representative, @news_item),
                   notice: 'News item was successfully created.'
@@ -55,23 +69,21 @@ class MyNewsItemsController < SessionController
     # )
     # puts @top_articles
     @top_articles = search_news_api(api_key)
-    puts @top_articles
+    Rails.logger.debug @top_articles
     if @top_articles.empty?
       redirect_to representative_news_items_path(@representative), alert: 'No articles found for this issue.'
     else
       render 'my_news_items/articles'
     end
-
   end
 
   def search_news_api(api_key)
     selected_name = Representative.find(params[:news_item][:representative_id]).name
-    puts selected_name
+    Rails.logger.debug selected_name
     uri = URI("https://newsapi.org/v2/everything?q=#{selected_name}%20#{params[:issue]}&language=en&sortBy=relevancy&pageSize=5&apiKey=#{api_key}")
     response = Net::HTTP.get_response(uri)
     parse_response = JSON.parse(response.body)['articles']
     @top_articles = parse_response
-
   end
 
   private
